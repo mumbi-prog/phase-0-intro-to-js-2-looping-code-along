65164b186aa4653844b98351a51b2ed9eb34a815
Sylvia Mumbi <mumbi099@gmail.com>
2023-05-12 03:17:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+//Task1
+function writeCards(namesArray, eventName) {
+    let messages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+  
+    return messages;
+}
+
+//Task2
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+}
